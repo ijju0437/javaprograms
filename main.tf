f5fb87e430b2352ace931c86e52699d9a0de1459
ijju <82495873+ijju0437@users.noreply.github.com>
2021-07-02 10:08:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,16 +7,9 @@ terraform {
   }
 }
 
-# Configure the AWS Provider
+# Configure the AWS Provider 
 provider "aws" {
-      ec2:
-         key_name: newkey
-         region: us-east-1
-         instance_type: t2.micro
-         image: ami-0ab4d1e9cf9a1215a
-         wait: yes
-         group: default
-         count: 1
-         vpc_subnet_id: subnet-47d29266
-         assign_public_ip: yes
+ access_key = "AKIAUEJWH3OXT7JYBIMT"
+ secret_key = "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"
+ region = "us-east-1"
 }
