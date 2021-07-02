44b24c11cc3eca4d4adbbd1f7d32b67eb1435366
ijju <82495873+ijju0437@users.noreply.github.com>
2021-07-02 10:10:08 +0530

    Update ubuntu.tf

--- ubuntu.tf
+++ ubuntu.tf
@@ -1,5 +1,11 @@
-provider "aws" {
- access_key = "AKIAUEJWH3OXT7JYBIMT"
- secret_key = "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"
- region = "us-east-1"
+resource "aws_instance" "myFirstInstance" {
+key_name: newkey
+         region: us-east-1
+         instance_type: t2.micro
+         image: ami-0ab4d1e9cf9a1215a
+         wait: yes
+         group: default
+         count: 1
+         vpc_subnet_id: subnet-47d29266
+         assign_public_ip: yes
 }
